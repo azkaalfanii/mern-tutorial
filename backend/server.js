21619e229fa41655eb9102850c89b6605dd83ed5
azkaalfanii <azkaalfani@gmail.com>
2022-07-15 21:50:41 +0700

    Register User

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,13 @@ const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
-
 connectDB()
-
 const app = express()
-
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
